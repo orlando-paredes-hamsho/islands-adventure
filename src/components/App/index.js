4b5b87e2d361b8e2d516318bba18ef98a697f1e6
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2021-05-08 16:00:52 -0500

    patch: integrate grid

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Grid from '../Grid';
 import './App.css';
 
 function App() {
   return (
-    <div className="app" data-testid="app" />
+    <div className="app" data-testid="app">
+      <Grid />
+    </div>
   );
 }
 
