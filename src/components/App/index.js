e122a7f46c1e7e900db5bbfed6b26753b191ae54
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2021-05-08 13:20:23 -0500

    patch: add linting

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,9 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="app" data-testid="app">
-    </div>
+    <div className="app" data-testid="app" />
   );
 }
 
