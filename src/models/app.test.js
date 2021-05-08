fc6b52144dc28925f766cb1ebd16befb1e0e294a
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2021-05-08 16:55:43 -0500

    test: store with values

--- src/models/app.test.js
+++ src/models/app.test.js
@@ -1,4 +1,5 @@
 import App from './app';
+import { generateGrid } from '../utils/grid';
 
 describe('App default values', () => {
   const app = new App();
@@ -10,5 +11,20 @@ describe('App default values', () => {
   });
   test('has an empty grid', () => {
     expect(app.grid).toEqual([]);
+  });
+});
+
+describe('App with param values', () => {
+  const height = 10;
+  const width = 5;
+  const app = new App(height, width);
+  test('has 0 height', () => {
+    expect(app.height).toEqual(height);
+  });
+  test('has 0 width', () => {
+    expect(app.width).toEqual(width);
+  });
+  test('has an empty grid', () => {
+    expect(app.grid).toEqual(generateGrid(height, width));
   });
 });
