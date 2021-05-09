ab9901ca892b8a1828cd627c306100c018da98b3
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2021-05-09 00:38:39 -0500

    patch: add toInt util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,8 @@
 const randomNumber = () => Math.floor(Math.random() * 100) + 1;
 
+export const toInt = (str) => {
+  const int = parseInt(str, 10);
+  return (Number.isNaN(int)) ? 0 : int;
+};
+
 export default randomNumber;
