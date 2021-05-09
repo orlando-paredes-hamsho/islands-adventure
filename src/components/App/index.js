ebaa97d6e03eb368b9b39c93c3870b30db74994a
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2021-05-09 00:14:17 -0500

    patch: integrate

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { observer } from 'mobx-react-lite';
 import { app } from '../../models/app';
 import Grid from '../Grid';
+import SizeInputContainer from '../SizeInputContainer';
 import SizeInput from '../SizeInput';
 import './styles.css';
 
-const App = () => (
+const App = observer(() => (
   <div className="app" data-testid="app">
-    <SizeInput name="height" />
-    <SizeInput name="width" />
+    <SizeInputContainer name="height">
+      <SizeInput name="height" value={app.height} changeAction={app.changeHeight} />
+    </SizeInputContainer>
+    <SizeInputContainer name="width">
+      <SizeInput name="width" value={app.width} changeAction={app.changeWidth} />
+    </SizeInputContainer>
     <Grid grid={app.grid} clickAction={app.flipCell} />
   </div>
-);
+));
 
 export default App;
