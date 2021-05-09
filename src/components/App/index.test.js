02123aac8a82b9ffd2413f3b1af0864ced2deb9b
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2021-05-08 21:54:18 -0500

    patch: consistent casing

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './index';
 
-test('renders App', () => {
+test('renders app', () => {
   render(<App />);
   const linkElement = screen.getByTestId('app');
   expect(linkElement).toBeInTheDocument();
